54693151d8685d3ae5c2a5f1222298f6a3e11145
Nuzair Rasheed <38219313+rednek46@users.noreply.github.com>
2020-05-29 20:10:37 +0530

    added enb and Menyoo trainer removal

--- GTAV Mod Switch.bat
+++ GTAV Mod Switch.bat
@@ -28,6 +28,10 @@ if ($ch -eq 'd'){
 	move $loc\dinput8.dll $loc\asi_backups\dinput8.dll.bak >$null 2>&1
 	move $loc\OpenIV.asi $loc\asi_backups\OpenIV.asi.bak >$null 2>&1
 	move $loc\ScriptHookV.dll $loc\asi_backups\ScriptHookV.dll.bak >$null 2>&1
+	move $loc\d3d11.dll $loc\asi_backups\d3dll.dll.bak >$null 2>&1
+	move $loc\Menyoo.asi $loc\asi_backups\Menyoo.asi.bak >$null 2>&1
+	move $loc\openCameraV.asi $loc\asi_backups\openCameraV.asi.bak >$null 2>&1
+	move $loc\d3dcompiler_46e.dll $loc\asi_backups\d3dcompiler_46e.dll.bak >$null 2>&1
 	rm $loc\commandline.txt >$null 2>&1
 
 }
@@ -39,6 +43,10 @@ elseif ($ch -eq 'e'){
 	move $loc\asi_backups\dinput8.dll.bak  $loc\dinput8.dll >$null 2>&1
 	move $loc\asi_backups\OpenIV.asi.bak  $loc\OpenIV.asi >$null 2>&1
 	move $loc\asi_backups\ScriptHookV.dll.bak $loc\ScriptHookV.dll >$null 2>&1
+	move $loc\asi_backups\d3dll.dll.bak $loc\d3d11.dll  >$null 2>&1
+	move $loc\asi_backups\Menyoo.asi.bak $loc\Menyoo.asi >$null 2>&1
+	move $loc\asi_backups\openCameraV.asi.bak $loc\openCameraV.asi >$null 2>&1
+	move $loc\asi_backups\d3dcompiler_46e.dll.bak $loc\d3dcompiler_46e.dll >$null 2>&1
 	New-Item $loc\commandline.txt >$null 2>&1
 	Set-Content $loc\commandline.txt '-scofflineonly'
 
